f2ad30d22da0c932fcc08bab56e656dcd58ed6e9
ercuco1415 <ercuco1415daw2@gmail.com>
2014-11-08 21:07:25 +0100

    Primer commit de mitja

---
+++ mitja.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	/*float suma=0;*/float suma = 0.0;
+	/*float mitja;*/float mitja=0.0;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf("Dòna la següent dada: ");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("Vols continuar (s/n): ");
+		/*scanf(" %c",&consulta);*/scanf("%s",&consulta);//modificació 
+	}
+	mitja=suma/numero_repeticions;
+	printf("La mitja és: %.2f\n",mitja);	
+	return 0;
+}
