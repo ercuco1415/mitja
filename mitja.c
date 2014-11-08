d8013277c3525970b1c1e643677915aef4d61852
ercuco1415 <ercuco1415daw2@gmail.com>
2014-11-08 21:24:52 +0100

    Tercer commit del programa mitja

--- mitja.c
+++ mitja.c
@@ -4,18 +4,18 @@ int main() {
 	/*float suma=0;*/float suma = 0.0;
 	/*float mitja;*/float mitja=0.0;
 	float nou_valor;
-	int numero_repeticions=0;
+	int num_repeticions=0;
 	char consulta='s';
 	
 	while(consulta=='s') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
-		numero_repeticions=numero_repeticions+1;
+		numero_repeticions=num_repeticions+1;
 		printf("Vols continuar (s/n): ");
 		/*scanf(" %c",&consulta);*/scanf("%s",&consulta);//modificació 
 	}
-	mitja=suma/numero_repeticions;
+	mitja=suma/num_repeticions;
 	printf("La mitja és: %.2f\n",mitja);	
 	return 0;
 }
