20df65e2e67dcf592d92e94e656e61085fe31966
erica <erica@erica-Lenovo-B570e.(none)>
2015-10-21 20:26:05 +0200

    Quart commit del projecte mitja

--- mitja.c
+++ mitja.c
@@ -3,19 +3,21 @@
 int main() {
 	/*float suma=0;*/float suma = 0.0;
 	/*float mitja;*/float mitja=0.0;
-	/*float nou_valor;*/float nou_valor=0.0;
-	int num_repeticions=0;
-	char consulta='s';
+	/*float nou_valor;*/
+	float nou_valor = 0.0;
+	int num_repeticions = 0;
+	char consulta = 's';
 	
 	while(consulta=='s') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
-		suma=suma+nou_valor;
-		numero_repeticions=num_repeticions+1;
+		suma = suma + nou_valor;
+		num_repeticions = num_repeticions + 1;
 		printf("Vols continuar (s/n): ");
-		/*scanf(" %c",&consulta);*/scanf("%s",&consulta);//modificació 
+		/*scanf(" %c",&consulta);*/
+		scanf("%s",&consulta);//modificació 
 	}
-	mitja=suma/num_repeticions;
+	mitja = suma/num_repeticions;
 	printf("La mitja és: %.2f\n",mitja);	
 	return 0;
 }
